31d263e9b1372c5b57c6b4d05db6a5eb18d6f9a0
Martin F <martin@luckycloud.de>
2022-10-17 16:18:32 +0200

    Update Configuration.php
    Add new way to inject Root node

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -39,8 +39,9 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ldap_tools');
+        $treeBuilder = new TreeBuilder('ldap_tools');
+        $rootNode = $treeBuilder->getRootNode();
+        //$rootNode = $treeBuilder->root('ldap_tools');
         $this->addMainSection($rootNode);
         $this->addDoctrineSection($rootNode);
         $this->addGeneralSection($rootNode);
